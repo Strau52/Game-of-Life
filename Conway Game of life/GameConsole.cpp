2b4c6e3823c1da56983b2c9833d10e948f7b7c13
Strausz <kirito1169@gmail.com>
2021-10-01 00:30:25 +0200

    Created the skeleton for the GameConsole class.

--- Conway Game of life/GameConsole.cpp
+++ Conway Game of life/GameConsole.cpp
@@ -1,1 +1,28 @@
 #include "GameConsole.h"
+
+GameConsole::GameConsole()
+{
+
+}
+
+void GameConsole::Write()
+{
+
+}
+
+void GameConsole::MainMenu()
+{
+
+}
+
+void GameConsole::GameSettingsInputs(const int& menu) 
+{
+
+}
+
+std::vector<std::vector<std::string>> GameConsole::VectorSplitter(const std::string& str)
+{
+    std::vector<std::vector<std::string>> outerVector;
+
+    return outerVector;
+}
