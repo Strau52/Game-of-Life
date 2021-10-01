ae6f6719754e97eeb132038391bfd4e246330ce9
Strausz <kirito1169@gmail.com>
2021-10-01 17:01:42 +0200

    Created and Implemented the Main menu.

--- Conway Game of life/GameConsole.cpp
+++ Conway Game of life/GameConsole.cpp
@@ -2,7 +2,7 @@
 
 GameConsole::GameConsole()
 {
-
+    Game mGame;
 }
 
 void GameConsole::Write()
@@ -12,7 +12,20 @@ void GameConsole::Write()
 
 void GameConsole::MainMenu()
 {
+    do {
+        system("CLS");
+        std::cout << "Choose a field creation mode!!\n";
+        std::cout << "1 - Square field, automatic probability\n";
+        std::cout << "2 - Square field, manual probability\n";
+        std::cout << "3 - Rectangle field, automatic probability\n";
+        std::cout << "4 - Rectangle field, manual probability\n";
+        std::cout << "5 - Rectangle field, cell pattern with vector\n";
+        std::cout << "6 - Quit\n";
+        std::cout << "My choice:";
+        std::cin >> mChoice;
+    } while (!((mChoice > 0) && (mChoice < 6)));
 
+    GameSettingsInputs(mChoice);
 }
 
 void GameConsole::GameSettingsInputs(const int& menu) 
